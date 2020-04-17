0baea03e9cb675f54d55ef621e5474d633f0f0fc
RodrigoAGM <rod-gm@hotmail.com>
2020-04-17 00:51:25 -0500

    :construction: Added city class

---
+++ server/city.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class City:
+    def __init__(self, name:str, x:float, y:float):
+        self.name = name
+        self.x = x
+        self.y = y
+    
+    def calculateDistance(self, endCity:City):
+
+        xDistance = abs(self.x - endCity.x)
+        yDistance = abs(self.y - endCity.y)
+
+        distance = np.sqrt((xDistance ** 2) + (yDistance ** 2))
+
+        return distance
+    
+    def __repr__(self):
+        return self.name + " (" + str(self.x) + "," + str(self.y) + ")"
