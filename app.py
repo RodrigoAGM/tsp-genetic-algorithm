02e40fca1a5cf049d5a66d96cff9a63bf719ce9f
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-18 18:28:33 -0500

    :tada: get client inputs and order with that

--- app.py
+++ app.py
@@ -15,24 +15,31 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/hello')
 @cross_origin()
 def hello():
-    return 'Hello, World!'
+    return 'Hello from Enzo Lizama & Rodrigo Guadalupe!'
 
 
 @app.route('/calculate_route', methods=['POST'])
 @cross_origin()
 def calculate_route():
     post_data = request.data.decode("utf-8")
-    cities = json.loads(post_data)
+    data = json.loads(post_data)
+    cities = data["cities"]
+    generations_number = int(data["generationNumber"])
+    mutation_probability = float(data["mutationProbability"])
+    initial_poblation = int(data["initialPoblation"])
+    elite_size = int(data["eliteSize"])
     cities = [c for c in cities]
 
     start = time.time()
     cityList = generateCityList(cities)
-    route, distance = geneticAlgorithm(cityList, 500, 0.05, 100, 20)
+    route, distance = geneticAlgorithm(cityList=cityList,
+                                       nGenerations=generations_number,
+                                       mutationProb=mutation_probability / 100,
+                                       initialPopulation=initial_poblation,
+                                       eliteSize=elite_size)
     route = generateDictArray(route)
     end = time.time()
-    # TODO: calculate routes and await for response
-    print(route)
-    return {"time": round(end-start,2), "route": route, "distance": round(distance,2)}
+    return {"time": round(end-start, 2), "route": route, "distance": round(distance, 2)}
 
 
 if __name__ == "__main__":
