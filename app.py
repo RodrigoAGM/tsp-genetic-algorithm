26bd3274fb9db69063283674d7179d078a5a2d00
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-18 15:14:19 -0500

    [U] get request and response

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+import json
+from server.city import City
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route('/hello')
+@cross_origin()
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/calculate_route', methods=['POST'])
+@cross_origin()
+def calculate_route():
+    post_data = request.data.decode("utf-8")
+    cities = json.loads(post_data)
+    cities = [c for c in cities]
+    # TODO: calculate routes and await for response
+    print(cities)
+    return {"time": 1213.12, "cities": cities, "distance": 121}
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
--- server/__init__.py
+++ server/__init__.py
@@ -1,13 +1,24 @@
-import os
+# import os
 
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
+# from flask import Flask, request
+# import json
+# from server.city import City
+# app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+# @app.route('/hello')
+# def hello():
+#     return 'Hello, World!'
+
+
+# @app.route('/calculate_route', methods=['POST'])
+# def calculate_route():
+#     post_data = request.data.decode("utf-8")
+#     cities = json.dumps(post_data)
+#     cities = [City(c['id'], c['x'], c['y']) for p in cities]
+#     print(cities)
+#     return {"time": 1213.12, "cities": cities, "distance": 121}
+
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0')
