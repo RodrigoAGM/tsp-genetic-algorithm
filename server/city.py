14be7843ffed8c0eed35acbacdd6fb0a2c66b7f8
Enzo Lizama <lizama.enzo@gmail.com>
2020-04-18 19:52:36 -0500

    :bulb: Documenting code for city and gen

--- server/city.py
+++ server/city.py
@@ -1,16 +1,34 @@
 import numpy as np
 
-#This is the class that represents a city with a name and (x,y) coordinates.
+# This is the class that represents a city with a name and (x,y) coordinates.
+
 
 class City:
-    def __init__(self, name:str, x:float, y:float):
+    """
+    Represents a city to visit for the traveler salesman into the map
+
+    Attributes
+    ----------
+    name: str
+        The name of the city
+    x: int
+        The horizontal coordinate into the map for the city
+    x: int
+        The vertical coordinate into the map for the city
+    """
+
+    def __init__(self, name: str, x: float, y: float):
         self.name = name
         self.x = x
         self.y = y
-    
+
     def calculateDistance(self, endCity):
         '''
         Method used to calculate the euclidean distance between this and another city
+        Parameters
+        ----------
+        endCity : City
+            The end city
         '''
 
         xDistance = abs(self.x - endCity.x)
@@ -19,6 +37,7 @@ class City:
         distance = np.sqrt((xDistance ** 2) + (yDistance ** 2))
 
         return distance
-    
+
     def __repr__(self):
+        # Mapper for the City object
         return self.name + " (" + str(self.x) + "," + str(self.y) + ")"
