cebc8f22b7e84be63b8b2b431f06e28e7b9cf21a
RodrigoAGM <rod-gm@hotmail.com>
2020-05-07 21:02:28 -0500

    :construction: Add energy functions

--- server/simulated_annealing.py
+++ server/simulated_annealing.py
@@ -1,6 +1,7 @@
 from city import City
 from typing import List
 import math
+import random
 
 class SimulatedAnnealing(object):
 
@@ -18,6 +19,26 @@ class SimulatedAnnealing(object):
         self.bestRoute = None
         self.bestEnergy = float("Inf")
 
+
+    def generateRandomPath(self):
+
+        listnum = [i for i in range(0, self.N)]
+        return random.sample(listnum, self.N)
+
+    def calculateEnergy(self, index1:int, index2:int):
+
+        city1, city2 = self.cityList[index1], self.cityList[index2]
+        return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
     
+    def calculatePathEnergy(self, path:List[int]):
+
+        totalEnergy = 0
+
+        for i in range(0, self.N - 1):
+
+            endIndex = 0 if i == (self.N - 1) else i + 1
+            totalEnergy += self.dist(path[i], path[endIndex])
+
+        return totalEnergy
 
 
